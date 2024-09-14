2e51b14d0fc5cb966e9545281d2ec7474053a368
Abdul Waheed <ab.waheed1722@gmail.com>
2024-09-14 16:45:03 +0300

    font export removed

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Montserrat } from 'next/font/google'
 import './globals.css'
 
-export const mont = Montserrat({
+const mont = Montserrat({
     subsets: ['latin'],
     display: 'swap',
 })
