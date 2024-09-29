a078dc1cf0513c2dab7b7262aea07f4970319b0a
Ab. Waheed <ab.waheed1722@gmail.com>
2024-09-30 02:27:26 +0300

    google analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import { GoogleAnalytics } from '@next/third-parties/google'
 import { Montserrat } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/custom/header'
@@ -31,6 +32,7 @@ export default function RootLayout({
                     <Footer />
                 </ThemeProvider>
             </body>
+            <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_ANALYTICS_ID ?? ''} />
         </html>
     )
 }
